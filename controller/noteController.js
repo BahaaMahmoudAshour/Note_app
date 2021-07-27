86c2a5e41f98d060abee69a6f770395a910e7662
Bahaa Ashour <Bahaamahmoudashour@gmail.com>
2021-07-27 11:33:27 +0200

    added add, update and delete functions to controller and route

--- controller/noteController.js
+++ controller/noteController.js
@@ -1,3 +1,15 @@
 exports.getAllNotes = (req , res)=>{
     res.send("get all notes ya 7ag");
 }
+
+exports.saveNotes = (req , res)=>{
+    res.send("Saved note  ya 7ag");
+}
+
+exports.updateNote = (req , res)=>{
+    res.send("Updated note  ya 7ag");
+}
+
+exports.deleteNote = (req , res)=>{
+    res.send("Deleted note  ya 7ag");
+}
--- route/noteRoute.js
+++ route/noteRoute.js
@@ -3,5 +3,9 @@ const router = express.Router();
 var noteContl= require('../controller/noteController');
 
 router.get('/notes' , noteContl.getAllNotes)
+router.post('/notes/save' , noteContl.saveNotes)
+router.put('/notes/update' , noteContl.updateNote)
+router.delete('/notes/delete' , noteContl.deleteNote)
+
 
 module.exports = router
