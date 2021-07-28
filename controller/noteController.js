f42e7e2a3955c907163f096806a86888ceb771a3
Bahaa Ashour <Bahaamahmoudashour@gmail.com>
2021-07-28 09:06:43 +0200

    cleanup

--- controller/noteController.js
+++ controller/noteController.js
@@ -1,23 +1,30 @@
 const { json } = require('body-parser');
 var generator = require('../utill/generator')
 var memstorage = require('../utill/memory_storage')
+var model = require('../model/note_model')
 
 
 exports.getAllNotes = (req , res)=>{
-    var seqId   = generator.generate();      
-    memstorage.store.setItem(seqId,"1st key ..."); 
-    var seqId_2   = generator.generate();   
-    memstorage.store.setItem(seqId_2,"2nd key ...");
-    
-    var keys = memstorage.getkeys(memstorage.store);
     var values = memstorage.getValues(memstorage.store);
-    console.log("Values = "+ JSON.stringify(values));// => AAB - 001
-    // => AAB - 001
-    res.send("get all notes keys "+JSON.stringify(keys));
+    console.log("Values = " +JSON.stringify(values));
+    return res.status(200).send(JSON.stringify(values));
 }
 
 exports.saveNotes = (req , res)=>{
-    res.send("Saved note  ya 7ag");
+    var seqId   = generator.generate();
+    var title =req.body.title;
+    var content =req.body.content;
+    var ctreatedBy ="admin";      
+    var ctreatedOn =new Date();      
+
+    var title =req.body.title;        
+    if (!title | !content){
+        return res.status(500).send({ error: "title and content shouldn't be empty"})
+
+    }  
+    var noteobj = new model.Note(seqId , title, content, ctreatedBy ,ctreatedOn);  
+    memstorage.store.setItem(seqId,noteobj); 
+    return res.status(201).send("Saved note successfully");
 }
 
 exports.updateNote = (req , res)=>{
